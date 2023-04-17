773c3b5ff56d279b26f7a9ddc02e3100c4f46f5b
arvydasg <azegaspa@gmail.com>
2023-04-17 07:55:39 +0300

    fixed timezones

--- stackunderflow_app/models.py
+++ stackunderflow_app/models.py
@@ -2,8 +2,12 @@ from datetime import datetime
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
+import pytz
 
 db = SQLAlchemy()
+
+tz = pytz.timezone("Europe/Vilnius")
+now = datetime.now(tz)
 
 
 class Users(db.Model, UserMixin):
@@ -12,7 +16,7 @@ class Users(db.Model, UserMixin):
     email = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     profile_image = db.Column(db.String(20), default="default.jpg")
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(tz))
 
 
 class Question(db.Model):
@@ -23,9 +27,9 @@ class Question(db.Model):
     author = db.relationship(
         "Users", backref="questions"
     )  # allows to access the name of the author instead of id
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    created_at = db.Column(db.DateTime, default=datetime.now(tz))
     modified_at = db.Column(
-        db.DateTime, default=None, onupdate=datetime.utcnow
+        db.DateTime, default=None, onupdate=datetime.now(tz)
     )  # onupdate method allows to specify an SQL expression that will
     # be executed when the row is updated
 
@@ -37,16 +41,17 @@ class Answer(db.Model):
     author = db.relationship("Users")
     question_id = db.Column(db.Integer, db.ForeignKey("question.id"))
     question = db.relationship("Question", backref=db.backref("answers", lazy=True))
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    created_at = db.Column(db.DateTime, default=datetime.now(tz))
     modified_at = db.Column(db.DateTime)
 
     likes = db.Column(db.Integer, default=0)
     dislikes = db.Column(db.Integer, default=0)
 
+    # only update the "modified_at" field when content field is updated, not when liked/disliked
     @validates("content")
     def update_modified_at(self, key, content):
         if self.content != content:
-            self.modified_at = datetime.utcnow()
+            self.modified_at = datetime.now(tz)
         return content
 
 
