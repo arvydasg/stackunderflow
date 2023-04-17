0b03425d65dd97c25ec9d6f123f4433778eb736c
arvydasg <azegaspa@gmail.com>
2023-04-17 08:00:13 +0300

    added error pages

--- stackunderflow_app/app.py
+++ stackunderflow_app/app.py
@@ -53,6 +53,15 @@ def create_app():
     def route_index():
         return render_template("index.html")
 
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return (render_template("errors/404.html"), 404)
+
+    # Internal server error
+    @app.errorhandler(500)
+    def page_not_found(e):
+        return render_template("errors/500.html"), 500
+
     return app
 
 
